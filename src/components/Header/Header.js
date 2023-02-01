e62dc6c0c1be75f6b11a8b24246904511c50ca4e
sudhanshugautam2911 <gautamsudhanshu2911@gmail.com>
2023-02-01 21:41:33 +0530

    ADD:  Header Bottom and side navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,26 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { logo } from '../assets/index'
+import { logo } from '../../assets/index'
 import LocationIcon from '@mui/icons-material/LocationOnOutlined';
 import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
 import SearchIcon from '@mui/icons-material/SearchOutlined';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { items } from '../constants';
+import { items } from '../../constants';
+import HeaderBottom from './HeaderBottom';
 
 const Header = () => {
 
     const [showAll, setShowAll] = useState(false);
+    const [search, setSearch] = useState('All')
 
+          //I was trying use ref method to hide list item but not working 
+    // const ref = useRef();
+    // useEffect(() => {
+    //     document.body.addEventListener("click", (e) => {
+    //         if (e.target.contains(ref.current)) {
+    //             setShowAll(false);
+    //         }
+    //     })
+    // }, [])
 
 
     useEffect(() => {
@@ -26,31 +37,32 @@ const Header = () => {
 
     return (
         <div>
-            <div className='w-full bg-amazon_blue text-white px-4 py-3 flex items-center'>
+            <div className='w-full bg-amazon_blue text-white flex items-center p-1 px-2'>
 
                 {/* Left side */}
                 <div className='headerHover'>
-                    <img className='w-24 mt-2' src={logo} alt="Amazon" />
+                    <img className='w-24' src={logo} alt="Amazon" />
                 </div>
 
                 <div className='headerHover'>
                     <LocationIcon />
-                    <p className='flex flex-col text-sm font-light text-lightText'>Deliver to <span className='font-semibold text-whiteText'>Oman</span></p>
+                    <p className='flex flex-col text-sm font-light text-lightText'>Deliver to <span className='font-semibold text-whiteText -mt-1'>Oman</span></p>
                 </div>
 
                 {/* mid search part */}
                 <div className="h-10 rounded-md flex flex-grow relative mx-2">
-                    <span onClick={() => setShowAll(!showAll)} className='w-14 h-full text-sm flex items-center justify-center text-[#6a6155] hover:text-amazon_blue bg-gray-200 hover:bg-gray-300  cursor-pointer rounded-tl-md rounded-bl-md px-7'>
-                        All<span></span>
+                    <span  onClick={() => setShowAll(!showAll)} className='w-auto h-full text-xs flex items-center justify-center text-[#6a6155] hover:text-amazon_blue bg-gray-200 hover:bg-gray-300  cursor-pointer rounded-tl-md rounded-bl-md px-2'>
+                        {search}<span></span>
                         <ArrowDropDownOutlinedIcon />
                     </span>
                     {
                         showAll && (
-                            <div>
+                            <div >
                                 <ul className='absolute w-48 h-80 top-10 left-0 overflow-y-scroll overflow-x-hidden bg-white border-[1px] text-black text-md font-titleFont flex-col gap-1 z-50 border-amazon_blue rounded-lg'>
 
                                     {items.map((item, index) => (
-                                        <li key={index} className='hover:text-white hover:bg-[#838383] p-1 cursor-pointer'>{item}</li>
+                                        <li key={index} onClick={() => setSearch(item)} className='hover:text-white hover:bg-[#838383] p-1 cursor-pointer'>{item}</li>
+
                                     ))}
 
                                 </ul>
@@ -77,15 +89,16 @@ const Header = () => {
                     <p className='text-xs font-light text-lightText'>Returns</p>
                     <p className='text-sm font-semibold -mt-1 text-whiteText'>
                         & Orders
-    
+
                     </p>
                 </div>
-                <div className='flex items-start justify-center headerHover relative'>
-                    <ShoppingCartIcon/>
-                    <p className='text-sm font-semibold b-6 text-whiteText mt-3'>Cart <span className='absolute text-xs left-6 -top-2 font-semibold bg-amazon_yellow rounded-full p-1 h-4 flex items-center justify-center text-amazon_blue'>0</span></p>
+                <div className='flex items-start items-center justify-center headerHover relative'>
+                    <ShoppingCartIcon />
+                    <p className='text-sm font-semibold b-6 text-whiteText mt-3'>Cart <span className='absolute text-xs left-6 top-0 font-semibold bg-amazon_yellow rounded-full p-1 h-4 flex items-center justify-center text-amazon_blue'>0</span></p>
                 </div>
-                
+
             </div>
+            <HeaderBottom />
         </div>
     )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
 export const items = [
+    "All",
     'Amazon Devices',
     'Amazon Fashion',
     'Amazon Fresh',
